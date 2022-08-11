c4f536c3be38dc4ea102fe9224da271b030fb0be
Anita <anitaisbeta@gmail.com>
2022-08-11 13:55:30 +0100

    updates

--- src/app/shared/resume/resume.component.ts
+++ src/app/shared/resume/resume.component.ts
@@ -6,39 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent  {
-  createSol = {
-    one: "-Determine the structure and design of web pages",
-    two: "-Developing features to enhance the user experience and accessibility",
-    three: "-Striking a balance between functional and aesthetics from designs Ensuring web design is optimized for smartphones",
-    four: "-Maintaining brand consistency throughout the design"
-  }
-
-  pharezTech = {
-    one: "-Write reusable codes for Redeemed Christian Church of God, Masha-Surulere website",
-    two: "-Write reusable codes for Messaging Fintech Services Application",
-    three: "-Create new modules for Redeemed Christian Church of God, Masha-Surulere website",
-    four: "-Create new modules for Messaging Fintech Services",
-    five: "-Application Work with UI/UX team to handle new projects",
-    six: "-Consume API endpoints for app usability & functionality"
-  }
-
-  lepsta = {
-    one: "-Find sponsors for EngineOne activities in West & Eastern Africa.",
-    two: "-Identify opportunities to expand the community through partnerships and entering new cities",
-    three: "-Analyze the market to gain insights into how to improve performance and maximize growth of the community",
-    four: "-Represent the region (West & Eastern Africa) in tech events",
-    five: "-Define the community target state, work plan and work streams focusing on impact together with the global champion and team.",
-    six: "-Support local groups in implementation, track progress and communicate to the global admin team (Guidione and Kabelo)",
-    seven: "-Identify performance gaps and initiate and roll-out improvement projects.",
-  }
-
-  techville = {
-    one: "-Create Figma files for visual layout of iOS, Android and web pages Graphics design, UI and UX development into customer specifications",
-    two: "-Come up with simple logo designs where necessary",
-    three: "-Implement and maintain high-quality SEO policies and incorporate them with content produced for the website",
-    four: "-Implement and maintain high-quality SEO policies and incorporate them with content produced for the website",
-    five: "-Work with UX developers in creating mock-ups for proposed project pages and update them as needed.",
-    six: "-Interpret and convert UI designs into websites using Wordpress CMS, html and JavaScript.",
-  }
   constructor() { }
+
+  phareztechs = [
+    {id: 1, job: "Write reusable codes for Redeemed Christian Church of God, Masha-Surulere website"},
+    {id: 2, job: "Write reusable components for Messaging Fintech Services Application"},
+    {id: 3, job: "Create new modules for Redeemed Christian Church of God, Masha-Surulere website"},
+    {id: 4, job: "Create new modules for Messaging Fintech Services"},
+    {id: 5, job: "Application Work with UI/UX team to handle new projects"},
+    {id: 6, job: "Consume API endpoints for app usability & functionality"}
+  ];
+
+  createsolutions = [
+    { id: 1, job: "Determine the structure and design of web pages" },
+    { id: 2, job: "Developing features to enhance the user experience and accessibility" },
+    { id: 3, job: "Striking a balance between functional and aesthetics from designs Ensuring web design is optimized for smartphones"},
+    { id: 4, job: "Maintaining brand consistency throughout the design"}
+  ];
+
+  lepstas = [
+    { id: 1, job: ""},
+    { id: 1, job: "Find sponsors for EngineOne activities in West & Eastern Africa."},
+    { id: 2, job: "Identify opportunities to expand the community through partnerships and entering new cities"},
+    { id: 3, job: "Analyze the market to gain insights into how to improve performance and maximize growth of the community"},
+    { id: 4, job: "Represent the region (West & Eastern Africa) in tech events"},
+    { id: 5, job: "Define the community target state, work plan and work streams focusing on impact together with the global champion and team."},
+    { id: 6, job: "Support local groups in implementation, track progress and communicate to the global admin team (Guidione and Kabelo)"},
+    { id: 6, job: "Identify performance gaps and initiate and roll-out improvement projects."}
+  ];
+
+  techvilles = [
+    {id: 1, job: "-Create Figma files for visual layout of iOS, Android and web pages Graphics design, UI and UX development into customer specifications"},
+    {id: 2, job: "Come up with simple logo designs where necessary"},
+    {id: 3, job: "Implement and maintain high-quality SEO policies and incorporate them with content produced for the website"},
+    {id: 4, job: "Implement and maintain high-quality SEO policies and incorporate them with content produced for the website"},
+    {id: 5, job: "Work with UX developers in creating mock-ups for proposed project pages and update them as needed."},
+    {id: 6, job: "Interpret and convert UI designs into websites using Wordpress CMS, html and JavaScript."}
+  ]
 }
