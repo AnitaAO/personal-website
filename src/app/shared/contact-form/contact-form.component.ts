93c65cdad5fd2d6ff24371b6d3980dd496a73b91
Anita <anitaisbeta@gmail.com>
2022-08-13 23:42:38 +0100

    update contact

--- src/app/shared/contact-form/contact-form.component.ts
+++ src/app/shared/contact-form/contact-form.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
   location = {
     address: "16a Fola Jinadu Cres, Gbagada, Lagos",
     email: "amarachi@amarachi.dev",
     phone: "+234 8065188884"
+  }
+
+  log(x: any) {console.log(x)};
+
+
+  submit(f: any){
+    f.value;
   }
 
   constructor() { }
